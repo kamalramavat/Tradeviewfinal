d7da63e758b7f93679fef7fbeb00a980556915a5
Kamal Ramavat <122508022+kamalramavat@users.noreply.github.com>
2023-11-03 19:06:39 +0530

    Add files via upload

---
+++ TradingViewWidget.jsx
@@ -0,0 +1,61 @@
+// TradingViewWidget.jsx
+"use client"
+// TradingViewWidget.jsx
+
+import React, { useEffect, useRef } from 'react';
+
+let tvScriptLoadingPromise;
+
+export default function TradingViewWidget() {
+  const onLoadScriptRef = useRef();
+
+  useEffect(
+    () => {
+      onLoadScriptRef.current = createWidget;
+
+      if (!tvScriptLoadingPromise) {
+        tvScriptLoadingPromise = new Promise((resolve) => {
+          const script = document.createElement('script');
+          script.id = 'tradingview-widget-loading-script';
+          script.src = 'https://s3.tradingview.com/tv.js';
+          script.type = 'text/javascript';
+          script.onload = resolve;
+
+          document.head.appendChild(script);
+        });
+      }
+
+      tvScriptLoadingPromise.then(() => onLoadScriptRef.current && onLoadScriptRef.current());
+
+      return () => onLoadScriptRef.current = null;
+
+      function createWidget() {
+        if (document.getElementById('tradingview_1eccb') && 'TradingView' in window) {
+          new window.TradingView.widget({
+            width: "2560",
+            height: "1080",
+            symbol: "NASDAQ:AAPL",
+            interval: "30",
+            timezone: "Asia/Kolkata",
+            theme: "light",
+            style: "1",
+            locale: "en",
+            enable_publishing: false,
+            allow_symbol_change: true,
+            container_id: "tradingview_1eccb"
+          });
+        }
+      }
+    },
+    []
+  );
+
+  return (
+    <div className='tradingview-widget-container'>
+      <div id='tradingview_1eccb' />
+      <div className="tradingview-widget-copyright">
+        <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a>
+      </div>
+    </div>
+  );
+}
